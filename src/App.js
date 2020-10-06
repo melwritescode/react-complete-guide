69163db61d535c4a5a70ecbf5d2ddd11343ee607
Melanie Williams <melaniep518@gmail.com>
2020-10-06 10:53:15 -0600

    add state to main App component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,36 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Melanie', age: 30 },
+      { name: 'Mike', age: 31 },
+      { name: 'John', age: 32 },
+    ],
+  };
+
+  handleSwitchNames = () => {
+    this.setState({
+      persons: [
+        { name: 'Bambi', age: 4 },
+        { name: 'Olive', age: 2 },
+        { name: 'Ava', age: 6 },
+      ],
+    });
+  };
+
   render() {
+    const { persons } = this.state;
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Melanie" age="30">
+        <button onClick={this.handleSwitchNames}>Switch Names</button>
+        <Person name={persons[0].name} age={persons[0].age}>
           My Hobbies: Hiking, Camping
         </Person>
-        <Person name="Mike" age="30" />
-        <Person name="Sandy" age="61" />
+        <Person name={persons[1].name} age={persons[1].age} />
+        <Person name={persons[2].name} age={persons[2].age} />
       </div>
     );
   }
